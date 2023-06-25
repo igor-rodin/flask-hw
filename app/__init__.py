4c47daae27809378ba226c3331f0231e57aa2a90
Igor Rodin <igreen1974@gmail.com>
2023-06-25 15:48:32 +0300

    Done products page

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import routs
+from app import fake_data
--- app/routs.py
+++ app/routs.py
@@ -1,6 +1,37 @@
+from flask import render_template
 from app import app
+from app.fake_data import site_menu
+from app.fake_data import catalog
 
 
 @app.route("/")
 def index():
-    return "<h1>Магазин одежды</h1>"
+    return render_template("index.html", title="Главная", menu_list=site_menu)
+
+
+@app.route("/products/")
+@app.route("/products/<string:category>")
+def products(category: str = "all"):
+    match category:
+        case "all":
+            products = list(catalog.values())
+        case _:
+            products = [catalog[category]]
+    return render_template(
+        "products.html", title="Наша продукция", menu_list=site_menu, products=products
+    )
+
+
+@app.route("/product/<int:id>")
+def get_product():
+    return render_template("products.html", title="{{}}", menu_list=site_menu)
+
+
+@app.route("/about")
+def about():
+    pass
+
+
+@app.route("/contacts")
+def contacts():
+    pass
