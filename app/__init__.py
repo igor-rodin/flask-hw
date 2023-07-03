4368783126dea6f6cf6c6d7bcd5a93aec2bb3f6c
igor-rodin <igreen1974@gmail.com>
2023-07-03 20:19:06 +0300

    Fix teacher's comments

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///students.db"
 app.config[
     "SECRET_KEY"
 ] = "P7gNZGcZJ$bf560b43e75678ed4976efd8437e80e709b83cf217e94bba95155b247cbcb1bd"
+app.config["STUDENTS_FILE"] = "app/lesson_3/data/students.json"
+
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
 
--- app/lesson_3/routs.py
+++ app/lesson_3/routs.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, url_for, flash
 from app import app, db
 from app.lesson_3.models import Student, Mark, User
 from app.lesson_3.forms import RegisterForm
+import json
 
 
 @app.route("/lesson-3/")
@@ -65,67 +66,21 @@ def init_db():
 
 @app.cli.command("fill-students-db")
 def fill_students_db():
-    student_1 = Student(
-        first_name="Иванов", last_name="Иванов", email="ivan@example.com", group=24
-    )
-    student_2 = Student(
-        first_name="Сергей", last_name="Петров", email="petr@example.com", group=24
-    )
-    student_3 = Student(
-        first_name="Лена", last_name="Павлова", email="lena@example.com", group=32
-    )
-    student_4 = Student(
-        first_name="Игорь", last_name="Иванов", email="igor@example.com", group=32
-    )
-    student_5 = Student(
-        first_name="Михаил", last_name="Козлов", email="mikle@example.com", group=32
-    )
-    student_6 = Student(
-        first_name="Вероника", last_name="Иванова", email="nika@example.com", group=24
-    )
+    json_file = app.config["STUDENTS_FILE"]
 
-    mark_1 = Mark(class_name="Теория поля", value=4, student=student_3)
-    mark_2 = Mark(class_name="Теория поля", value=5, student=student_4)
-    mark_3 = Mark(class_name="Теория поля", value=4, student=student_5)
-    mark_4 = Mark(class_name="История", value=4, student=student_4)
-    mark_5 = Mark(class_name="История", value=5, student=student_3)
-    mark_6 = Mark(class_name="История", value=5, student=student_5)
-    mark_7 = Mark(class_name="Химия", value=4, student=student_1)
-    mark_8 = Mark(class_name="Химия", value=3, student=student_2)
-    mark_9 = Mark(class_name="Химия", value=5, student=student_6)
-    mark_10 = Mark(class_name="Биология", value=3, student=student_1)
-    mark_11 = Mark(class_name="Биология", value=4, student=student_2)
-    mark_12 = Mark(class_name="Биология", value=4, student=student_6)
-    mark_13 = Mark(class_name="Общая физика", value=4, student=student_1)
-    mark_14 = Mark(class_name="Общая физика", value=5, student=student_2)
-    mark_15 = Mark(class_name="Общая физика", value=4, student=student_6)
-    mark_16 = Mark(class_name="Мат. анализ", value=4, student=student_4)
-    mark_17 = Mark(class_name="Мат. анализ", value=5, student=student_3)
-    mark_18 = Mark(class_name="Мат. анализ", value=3, student=student_5)
+    with open(json_file, mode="r", encoding="utf-8") as f:
+        students = json.load(f)
 
-    db.session.add_all(
-        [student_1, student_2, student_3, student_4, student_5, student_6]
-    )
-    db.session.add_all(
-        [
-            mark_1,
-            mark_2,
-            mark_3,
-            mark_4,
-            mark_5,
-            mark_6,
-            mark_7,
-            mark_8,
-            mark_9,
-            mark_10,
-            mark_11,
-            mark_12,
-            mark_13,
-            mark_14,
-            mark_15,
-            mark_16,
-            mark_17,
-            mark_18,
-        ]
-    )
+    for item in students:
+        student = Student(
+            first_name=item["first_name"],
+            last_name=item["last_name"],
+            email=item["email"],
+            group=item["group"],
+        )
+        for mark in item["marks"]:
+            student.marks.append(
+                Mark(class_name=mark["class_name"], value=mark["value"])
+            )
+        db.session.add(student)
     db.session.commit()
